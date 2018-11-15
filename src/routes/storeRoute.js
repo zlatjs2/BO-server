d516d0b78bf3a2f73fddb52aba5903e246bd6ae2
hohee <boycat87@gmail.com>
2018-11-15 17:42:57 +0900

    Logger.js

--- src/routes/storeRoute.js
+++ src/routes/storeRoute.js
@@ -1,50 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const async = require('async');
-const debug = require('debug')('BO-server:store');
-const name = 'storeRouter';
+
+const Logger = require('../util/LoggerUtil');
 
 router.get('/', (req, res, next) => {
   try {
     async.waterfall([
       (callback) => {
-      }, (callback) => {
-
-      }, (callback) => {
-
+        let responseText = 'Hello World!';
+        responseText += ' Requested at: ' + req.requestTime + '';
+        res.send(responseText);
       }
     ], (err, result) => {
-
-      console.log('##### waterfall error: ', error)
+      Logger.e(error);
     })
   } catch (error) {
-    console.log('#####  error: ', error)
+    Logger.e(error);
   }
 });
-
-router.get('/a_route_behind_paywall', (req, res, next) => {
-  try {
-    async.waterfall([
-      (callback) => {
-        if (!req.user.ttt) {
-          console.log('##### 2: ', 2);
-          next('route');    // 라우터 미들웨어 스택의 나머지 미들웨어 함수들을 건너뛴다.
-        }
-      }
-    ], (err, result) => {
-      console.log('##### waterfall error: ', error)
-    })
-  } catch (error) {
-    // 오류를 처리하도록 설정된 핸들러를 제외한 체인 내의 나머지 모든 핸들러를 건너 뛴다.
-    next(error);
-  }
-}, (req, res, next) => {
-  PaidContent.find((err, doc) => {
-    console.log('##### 3: ', 3);
-    if (err) return next(err);
-    res.json(doc);
-  });
-});
-
 
 module.exports = router;
---
+++ src/util/LoggerUtil.js
@@ -0,0 +1,56 @@
+const log4js = require('log4js');
+const dateFormat = require('dateformat');
+
+const LOG4JS_FILE_PATH = '/service/log/nodejs/BO_node.log';
+const ONE_DAY_ROLLING_PATTERN = '.yyyy-MM-dd'; // 하루단위 롤링.
+const ONE_HOUR_ROLLING_PATTERN = '.yyyy-MM-dd-hh';  // 시간단위 롤링.
+
+const TOKEN = 'test-user';
+
+log4js.configure({
+  pm2: true,
+  appenders: {
+    'BO_logfile': {
+      type: 'dateFile', filename: LOG4JS_FILE_PATH, pattern: ONE_DAY_ROLLING_PATTERN,
+      layout: {
+        type: 'messagePassThrough'
+      }
+    },
+    'BO_debug': {
+      type: 'console',
+      layout: {
+        type: 'coloured'
+      }
+    }
+  },
+  categories: { 
+    default: { appenders: ['BO_logfile'], level: 'debug' } ,
+    BO_debug: { appenders: ['BO_debug'], level: 'debug'}
+  }
+});
+
+const logger_file = log4js.getLogger('BO_logfile');
+const logger_debug = log4js.getLogger('BO_debug');
+const Logger = {};
+
+Logger.df = (log) => {
+  logger_file.debug(log);
+  Logger.d(log);
+};
+
+Logger.i = (s) => {
+  let prefix = `<${process.pid}> `;
+  logger_debug.info(prefix + s);
+}
+
+Logger.d = (s) => {
+  let prefix = `<${process.pid}> `;
+  logger_debug.debug(prefix + s);
+};
+
+Logger.e = (s) => {
+  let prefix = `<${process.pid}> `;
+  logger_debug.error(prefix + s);
+};
+
+module.exports = Logger;
