9fdff41717b21499b8dce0504691f5ff29c47c8e
zlatjs2 <zlatjs2@gmail.com>
2018-11-20 09:04:48 +0900

    perf: 메뉴관리 코드 수정

--- src/interactor/mapper/mysql/menu.js
+++ src/interactor/mapper/mysql/menu.js
@@ -6,41 +6,11 @@ const menuMapper = {};
 const TABLE = 'Menu';
 
 menuMapper.selMenuList = (params, callback) => {
-  let obj = Object.values(params);
   let and = '';
-
-  if (obj.length === 1) {
-    if (params.name) {
-      and += `and menuname like '%${params.name}%'`;
-    } else if (params.status) {
-      and += `and status='${params.status}'`;
-    } else if (params.type) {
-      and += `and mealtype='${params.type}'`;
-    }
-  } else if (obj.length === 2) {
-    if (params.name || params.status) {
-      and += `
-        and menuname like '%${params.name}%'
-        and status='${params.status}'
-      `;
-    } else if (params.name || params.type) {
-      and += `
-        and menuname like '%${params.name}%'
-        and mealtype='${params.type}'
-      `;
-    } else if (params.status || params.type) {
-      and += `
-        and status='${params.status}'
-        and mealtype='${params.type}'
-      `;
-    }
-  } else if (params.name || params.status || params.type) {
-    and += `
-      and menuname like '%${params.name}%'
-      and status='${params.status}'
-      and mealtype='${params.type}'
-    `;
-  }
+  
+  if (params.name) { and += `and menuname like '%${params.name}%'`; }
+  if (params.status) { and += `and status='${params.status}'`; }
+  if (params.type) { and += `and mealtype='${params.type}'`; }
 
   const query = `
     select * from ${TABLE}
